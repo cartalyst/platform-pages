d60a147d3a81b0c141cc665249cc6d14ffd6862d
Bruno G. <brunofgaspar1@gmail.com>
2013-01-29 01:39:11 +0000

    updating extension.php file

--- extension.php
+++ extension.php
@@ -200,6 +200,36 @@ return array(
 
 	/*
 	|--------------------------------------------------------------------------
+	| Permissions
+	|--------------------------------------------------------------------------
+	|
+	| List of permissions this extension has. These are shown in user
+	| management to build a graphical interface where permissions may be
+	| selected.
+	|
+	| The admin controllers state that permissions should follow the following
+	| structure:
+	|
+	|     vendor/extension::admin.controller@method
+	|
+	| For example:
+	|
+	|    platform/users::admin.usersController@getIndex
+	|    Platform\Users\Controllers\Admin\UsersController@getIndex
+	|
+	| These are automatically generated for controller routes however you are
+	| free to add your own permissions and check against them at any time.
+	|
+	| When writing permissions, if you put a 'key' => 'value' pair, the 'value'
+	| will be the label for the permission which is displayed when editing
+	| permissions.
+	|
+	*/
+
+	'permissions' => array(),
+
+	/*
+	|--------------------------------------------------------------------------
 	| Widgets
 	|--------------------------------------------------------------------------
 	|
