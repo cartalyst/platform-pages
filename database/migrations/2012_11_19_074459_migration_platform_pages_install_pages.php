4c746e958ea27409a741ffd77de6564446a25493
Ben Corlett <bencorlett@me.com>
2013-05-13 12:22:55 +1000

    Default page is now "home", not "welcome". Fixes #2.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- database/migrations/2012_11_19_074459_migration_platform_pages_install_pages.php
+++ database/migrations/2012_11_19_074459_migration_platform_pages_install_pages.php
@@ -59,15 +59,15 @@ class MigrationPlatformPagesInstallPages extends Migration {
 			$table->unique(array('page_id', 'group_id'));
 		});
 
-		// We'll go ahead and make a "Welcome" page,
-		// which will be the default for a Platform
+		// We'll go ahead and make a home page, which
+		// will be the default for a Platform
 		// installation
 		$page = new Page(array(
-			'name'       => 'Welcome',
-			'slug'       => 'welcome',
+			'name'       => 'Home',
+			'slug'       => 'home',
 			'visibility' => 'always',
 			'type'       => 'filesystem',
-			'file'       => 'pages/welcome',
+			'file'       => 'pages/home',
 			'enabled'    => true,
 		));
 		$page->save();
