80f0d0f443fef6545c155cb97c377c7067623386
Bruno G. <brunofgaspar1@gmail.com>
2013-06-22 04:33:57 +0100

    remove extra spaces

--- themes/admin/default/packages/platform/pages/views/form.blade.php
+++ themes/admin/default/packages/platform/pages/views/form.blade.php
@@ -44,140 +44,140 @@
 	<section class="page__content">
 
 		<fieldset>
-				<legend>{{ trans("platform/pages::form.{$pageSegment}.legend") }}</legend>
+			<legend>{{ trans("platform/pages::form.{$pageSegment}.legend") }}</legend>
 
-				{{-- Name --}}
-				<div class="control-group{{ $errors->first('name', ' error') }}" required>
-					<label class="control-label" for="name">{{ trans('platform/pages::form.name') }}</label>
+			{{-- Name --}}
+			<div class="control-group{{ $errors->first('name', ' error') }}" required>
+				<label class="control-label" for="name">{{ trans('platform/pages::form.name') }}</label>
+				<div class="controls">
+					<input type="text" name="name" id="name" value="{{{ Input::old('name', ! empty($page) ? $page->name : null) }}}" placeholder="{{ trans('platform/pages::form.name_help') }}" required>
+					{{ $errors->first('name', '<span class="help-inline">:message</span>') }}
+				</div>
+			</div>
+
+			{{-- Slug --}}
+			<div class="control-group{{ $errors->first('slug', ' error') }}" required>
+				<label class="control-label" for="slug">{{ trans('platform/pages::form.slug') }}</label>
+				<div class="controls">
+					<div class="input-prepend">
+						<span class="add-on">
+							{{ str_finish(URL::to('/'), '/') }}
+						</span>
+						<input type="text" name="slug" id="slug" value="{{{ Input::old('slug', ! empty($page) ? $page->slug : null) }}}" placeholder="{{ trans('platform/pages::form.slug_help') }}" required>
+					</div>
+					{{ $errors->first('slug', '<span class="help-inline">:message</span>') }}
+				</div>
+			</div>
+
+			{{-- Enabled --}}
+			<div class="control-group{{ $errors->first('enabled', ' error') }}" required>
+				<label class="control-label" for="enabled">{{ trans('platform/pages::form.status') }}</label>
+				<div class="controls">
+					<select name="enabled" id="enabled" required>
+						<option value="1"{{ Input::old('enabled', ! empty($page) ? (int) $page->enabled : 1) === 1 ? ' selected="selected"' : null }}>{{ trans('general.enabled') }}</option>
+						<option value="0"{{ Input::old('enabled', ! empty($page) ? (int) $page->enabled : 1) === 0 ? ' selected="selected"' : null }}>{{ trans('general.disabled') }}</option>
+					</select>
+					{{ $errors->first('enabled', '<span class="help-inline">:message</span>') }}
+				</div>
+			</div>
+
+			{{-- Type --}}
+			<div class="control-group{{ $errors->first('type', ' error') }}" required>
+				<label class="control-label" for="type">{{ trans('platform/pages::form.type') }}</label>
+				<div class="controls">
+					<select name="type" id="type">
+						<option value="database"{{ Input::old('type', ! empty($page) ? $page->type : 'database') == 'database' ? ' selected="selected"' : null }}>{{ trans('platform/content::form.database') }}</option>
+						<option value="filesystem"{{ Input::old('type', ! empty($page) ? $page->type : 'database') == 'filesystem' ? ' selected="selected"' : null }}>{{ trans('platform/content::form.filesystem') }}</option>
+					</select>
+					{{ $errors->first('type', '<span class="help-inline">:message</span>') }}
+				</div>
+			</div>
+
+			{{-- Visibility --}}
+			<div class="control-group{{ $errors->first('visibility', ' error') }}" required>
+				<label for="visibility" class="control-label">{{ trans('platform/pages::form.visibility.legend') }}</label>
+				<div class="controls">
+					<select name="visibility" id="visibility">
+						<option value="always"{{ Input::old('visibility', ! empty($page) ? $page->visibility : 'always') == 'always' ? ' selected="selected"' : null }}>{{ trans('platform/pages::form.visibility.always') }}</option>
+						<option value="logged_in"{{ Input::old('visibility', ! empty($page) ? $page->visibility : 'always') == 'logged_in' ? ' selected="selected"' : null }}>{{ trans('platform/pages::form.visibility.logged_in') }}</option>
+						<option value="admin"{{ Input::old('visibility', ! empty($page) ? $page->visibility : 'always') == 'admin' ? ' selected="selected"' : null }}>{{ trans('platform/pages::form.visibility.admin') }}</option>
+					</select>
+					{{ $errors->first('visibility', '<span class="help-inline">:message</span>') }}
+				</div>
+			</div>
+
+			{{-- Groups --}}
+			<div class="control-group{{ $errors->first('groups', ' error') }}" required>
+				<label for="groups" class="control-label">{{ trans('platform/pages::form.groups') }}</label>
+				<div class="controls">
+					<select name="groups[]" id="groups[]" multiple="multiple">
+					@foreach ($groups as $group)
+						<option value="{{ $group->id }}"{{ array_key_exists($group->id, $pageGroups) ? ' selected="selected"' : null }}>{{ $group->name }}</option>
+					@endforeach
+					</select>
+					{{ $errors->first('groups', '<span class="help-inline">:message</span>') }}
+				</div>
+			</div>
+
+
+			<div class="type-database{{ Input::old('type', ! empty($page) ? $page->type : 'database') == 'filesystem' ? ' hide' : null }}">
+
+				{{-- Templates --}}
+				<div class="control-group{{ $errors->first('template', ' error') }}" required>
+					<label class="control-label" for="template">{{ trans('platform/pages::form.template') }}</label>
 					<div class="controls">
-						<input type="text" name="name" id="name" value="{{{ Input::old('name', ! empty($page) ? $page->name : null) }}}" placeholder="{{ trans('platform/pages::form.name_help') }}" required>
-						{{ $errors->first('name', '<span class="help-inline">:message</span>') }}
+						<select name="template" id="template" required>
+						@foreach ($templates as $value => $name)
+							<option value="{{ $value }}"{{ Input::old('template', ! empty($page) ? $page->template : null) == $value ? ' selected="selected"' : null}}>{{ $name }}</option>
+						@endforeach
+						</select>
+						{{ $errors->first('template', '<span class="help-inline">:message</span>') }}
 					</div>
 				</div>
 
-				{{-- Slug --}}
-				<div class="control-group{{ $errors->first('slug', ' error') }}" required>
-					<label class="control-label" for="slug">{{ trans('platform/pages::form.slug') }}</label>
+				{{-- Section --}}
+				<div class="control-group{{ $errors->first('section', ' error') }}" required>
+					<label class="control-label" for="section">{{ trans('platform/pages::form.section') }}</label>
 					<div class="controls">
 						<div class="input-prepend">
-							<span class="add-on">
-								{{ str_finish(URL::to('/'), '/') }}
-							</span>
-							<input type="text" name="slug" id="slug" value="{{{ Input::old('slug', ! empty($page) ? $page->slug : null) }}}" placeholder="{{ trans('platform/pages::form.slug_help') }}" required>
+							<i class="add-on">@</i>
+							<input type="text" name="section" value="{{{ Input::old('section', ! empty($page) ? $page->section : null) }}}" placeholder="{{ trans('platform/pages::form.section_help') }}">
 						</div>
-						{{ $errors->first('slug', '<span class="help-inline">:message</span>') }}
+						{{ $errors->first('section', '<span class="help-inline">:message</span>') }}
 					</div>
 				</div>
 
-				{{-- Enabled --}}
-				<div class="control-group{{ $errors->first('enabled', ' error') }}" required>
-					<label class="control-label" for="enabled">{{ trans('platform/pages::form.status') }}</label>
+				{{-- Value --}}
+				<div class="control-group{{ $errors->first('value', ' error') }}" required>
+					<label class="control-label" for="value">{{ trans('platform/pages::form.value') }}</label>
 					<div class="controls">
-						<select name="enabled" id="enabled" required>
-							<option value="1"{{ Input::old('enabled', ! empty($page) ? (int) $page->enabled : 1) === 1 ? ' selected="selected"' : null }}>{{ trans('general.enabled') }}</option>
-							<option value="0"{{ Input::old('enabled', ! empty($page) ? (int) $page->enabled : 1) === 0 ? ' selected="selected"' : null }}>{{ trans('general.disabled') }}</option>
-						</select>
-						{{ $errors->first('enabled', '<span class="help-inline">:message</span>') }}
+						<textarea rows="10" name="value" id="value"{{{ Input::old('value', ! empty($page) ? $page->type : null) == 'database' ? ' required' : null }}}>{{ Input::old('value', ! empty($page) ? $page->value : null) }}</textarea>
+						{{ $errors->first('value', '<span class="help-inline">:message</span>') }}
 					</div>
 				</div>
 
-				{{-- Type --}}
-				<div class="control-group{{ $errors->first('type', ' error') }}" required>
-					<label class="control-label" for="type">{{ trans('platform/pages::form.type') }}</label>
+			</div>
+
+			<div class="type-filesystem{{ Input::old('type', ! empty($page) ? $page->type : 'database') == 'database' ? ' hide' : null }}">
+
+				{{-- File --}}
+				<div class="control-group{{ $errors->first('file', ' error') }}" required>
+					<label class="control-label" for="file">{{ trans('platform/pages::form.file') }}</label>
 					<div class="controls">
-						<select name="type" id="type">
-							<option value="database"{{ Input::old('type', ! empty($page) ? $page->type : 'database') == 'database' ? ' selected="selected"' : null }}>{{ trans('platform/content::form.database') }}</option>
-							<option value="filesystem"{{ Input::old('type', ! empty($page) ? $page->type : 'database') == 'filesystem' ? ' selected="selected"' : null }}>{{ trans('platform/content::form.filesystem') }}</option>
+						<select name="file" id="file" required>
+						@foreach ($files as $value => $name)
+							<option value="{{ $value }}"{{ Input::old('file', ! empty($page) ? $page->file : null) == $value ? ' selected="selected"' : null}}>{{ $name }}</option>
+						@endforeach
 						</select>
-						{{ $errors->first('type', '<span class="help-inline">:message</span>') }}
+						{{ $errors->first('file', '<span class="help-inline">:message</span>') }}
 					</div>
 				</div>
 
-				{{-- Visibility --}}
-				<div class="control-group{{ $errors->first('visibility', ' error') }}" required>
-					<label for="visibility" class="control-label">{{ trans('platform/pages::form.visibility.legend') }}</label>
-					<div class="controls">
-						<select name="visibility" id="visibility">
-							<option value="always"{{ Input::old('visibility', ! empty($page) ? $page->visibility : 'always') == 'always' ? ' selected="selected"' : null }}>{{ trans('platform/pages::form.visibility.always') }}</option>
-							<option value="logged_in"{{ Input::old('visibility', ! empty($page) ? $page->visibility : 'always') == 'logged_in' ? ' selected="selected"' : null }}>{{ trans('platform/pages::form.visibility.logged_in') }}</option>
-							<option value="admin"{{ Input::old('visibility', ! empty($page) ? $page->visibility : 'always') == 'admin' ? ' selected="selected"' : null }}>{{ trans('platform/pages::form.visibility.admin') }}</option>
-						</select>
-						{{ $errors->first('visibility', '<span class="help-inline">:message</span>') }}
-					</div>
-				</div>
+			</div>
 
-				{{-- Groups --}}
-				<div class="control-group{{ $errors->first('groups', ' error') }}" required>
-					<label for="groups" class="control-label">{{ trans('platform/pages::form.groups') }}</label>
-					<div class="controls">
-						<select name="groups[]" id="groups[]" multiple="multiple">
-							@foreach ($groups as $group)
-								<option value="{{ $group->id }}"{{ array_key_exists($group->id, $pageGroups) ? ' selected="selected"' : null }}>{{ $group->name }}</option>
-							@endforeach
-						</select>
-						{{ $errors->first('groups', '<span class="help-inline">:message</span>') }}
-					</div>
-				</div>
+		</fieldset>
 
-
-				<div class="type-database{{ Input::old('type', ! empty($page) ? $page->type : 'database') == 'filesystem' ? ' hide' : null }}">
-
-					{{-- Templates --}}
-					<div class="control-group{{ $errors->first('template', ' error') }}" required>
-						<label class="control-label" for="template">{{ trans('platform/pages::form.template') }}</label>
-						<div class="controls">
-							<select name="template" id="template" required>
-								@foreach ($templates as $value => $name)
-								<option value="{{ $value }}"{{ Input::old('template', ! empty($page) ? $page->template : null) == $value ? ' selected="selected"' : null}}>{{ $name }}</option>
-								@endforeach
-							</select>
-							{{ $errors->first('template', '<span class="help-inline">:message</span>') }}
-						</div>
-					</div>
-
-					{{-- Section --}}
-					<div class="control-group{{ $errors->first('section', ' error') }}" required>
-						<label class="control-label" for="section">{{ trans('platform/pages::form.section') }}</label>
-						<div class="controls">
-							<div class="input-prepend">
-								<i class="add-on">@</i>
-								<input type="text" name="section" value="{{{ Input::old('section', ! empty($page) ? $page->section : null) }}}" placeholder="{{ trans('platform/pages::form.section_help') }}">
-							</div>
-							{{ $errors->first('section', '<span class="help-inline">:message</span>') }}
-						</div>
-					</div>
-
-					{{-- Value --}}
-					<div class="control-group{{ $errors->first('value', ' error') }}" required>
-						<label class="control-label" for="value">{{ trans('platform/pages::form.value') }}</label>
-						<div class="controls">
-							<textarea rows="10" name="value" id="value"{{{ Input::old('value', ! empty($page) ? $page->type : null) == 'database' ? ' required' : null }}}>{{ Input::old('value', ! empty($page) ? $page->value : null) }}</textarea>
-							{{ $errors->first('value', '<span class="help-inline">:message</span>') }}
-						</div>
-					</div>
-
-				</div>
-
-				<div class="type-filesystem{{ Input::old('type', ! empty($page) ? $page->type : 'database') == 'database' ? ' hide' : null }}">
-
-					{{-- File --}}
-					<div class="control-group{{ $errors->first('file', ' error') }}" required>
-						<label class="control-label" for="file">{{ trans('platform/pages::form.file') }}</label>
-						<div class="controls">
-							<select name="file" id="file" required>
-								@foreach ($files as $value => $name)
-								<option value="{{ $value }}"{{ Input::old('file', ! empty($page) ? $page->file : null) == $value ? ' selected="selected"' : null}}>{{ $name }}</option>
-								@endforeach
-							</select>
-							{{ $errors->first('file', '<span class="help-inline">:message</span>') }}
-						</div>
-					</div>
-
-				</div>
-
-			</fieldset>
-
-		</section>
+	</section>
 
 	<footer class="page__footer">
 
@@ -185,10 +185,10 @@
 			<ul class="navigation navigation--inline-circle">
 				@if( ! empty($page) and $pageSegment != 'copy')
 				<li>
-					<a class="danger tip" data-placement="top" data-toggle="modal" data-target="#platform-modal-confirm" href="{{ URL::toAdmin("pages/delete/{$page->id}") }}" title="{{ trans('button.delete') }}"><i class="icon-trash"></i></a>
+					<a class="danger tip" data-placement="top" data-toggle="modal" data-target="#platform-modal-confirm" href="{{ URL::toAdmin("pages/delete/{$page->slug}") }}" title="{{ trans('button.delete') }}"><i class="icon-trash"></i></a>
 				</li>
 				<li>
-					<a class="tip" data-placement="top" href="{{ URL::toAdmin("pages/copy/{$page->id}") }}" title="{{ trans('button.copy') }}"><i class="icon-copy"></i></a>
+					<a class="tip" data-placement="top" href="{{ URL::toAdmin("pages/copy/{$page->slug}") }}" title="{{ trans('button.copy') }}"><i class="icon-copy"></i></a>
 				</li>
 				@endif
 				<li>
