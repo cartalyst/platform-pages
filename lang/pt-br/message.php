3e41c4652015aebf2be98180e380b6f4d09662a7
Igor Herson <igorhaf@gmail.com>
2013-08-30 11:45:46 -0300

    add brazilian portuguese translation

---
+++ lang/pt-br/message.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Part of the Platform application.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.  It is also available at
+ * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ * @package    Platform
+ * @version    2.0.0
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011 - 2013, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+return array(
+
+	// General messages
+	'not_found' => 'Página  [:id] não existe.',
+
+	// Success messages
+	'success' => array(
+		'create' => 'A página foi criada com sucesso.',
+		'update' => 'A página foi atualizada com sucesso.',
+		'delete' => 'A página foi excluida com sucesso.',
+	),
+
+	// Error messages
+	'error' => array(
+		'create' => 'Houve um problema ao criar a página. Por favor, tente novamente.',
+		'update' => 'Houve um problema ao editar a página. Por favor, tente novamente.',
+		'delete' => 'Houve um problema ao excluir a página. Por favor, tente novamente.',
+	),
+
+);
