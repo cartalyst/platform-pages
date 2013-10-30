e62514762503a105bfd7edd4f52da6369e3d75cc
Bruno Gaspar <brunofgaspar1@gmail.com>
2013-10-30 17:52:12 +0000

    small tweaks on the template view for the menu page type
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- themes/admin/default/packages/platform/pages/views/types/template.blade.php
+++ themes/admin/default/packages/platform/pages/views/types/template.blade.php
@@ -1,9 +1,9 @@
-<div class="form-group hide" data-item-type="page">
+<div class="form-group[? if type != 'page' ?] hide[? endif ?]" data-item-type="page">
 	<label class="control-label" for="[[ slug ]]_page_uri">Select a page</label>
 
 	<select data-item-form="[[ slug ]]" name="children[[[ slug ]]][page_uri]" id="[[ slug ]]_page_uri" class="form-control">
 		@foreach ($pages as $page)
-		<option value="{{ $page->id }}">/{{ $page->uri }}</option>
+		<option value="{{ $page->id }}"[? if page_uri == '{{ $page->id }}' ?] selected="selected"[? endif ?]>/{{ $page->uri }}</option>
 		@endforeach
 	</select>
 </div>
