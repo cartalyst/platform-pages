46ec3961f4027d2afa363c0f326e278b0418c6ee
Bruno Gaspar <brunofgaspar1@gmail.com>
2013-10-30 22:01:06 +0000

    update page type template view for underscore markup
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- themes/admin/default/packages/platform/pages/views/types/template.blade.php
+++ themes/admin/default/packages/platform/pages/views/types/template.blade.php
@@ -1,9 +1,13 @@
-<div class="form-group[? if type != 'page' ?] hide[? endif ?]" data-item-type="page">
-	<label class="control-label" for="[[ slug ]]_page_uri">Select a page</label>
+<div class="form-group<% if (type != 'page') { %> hide<% } %>" data-item-type="page">
+	<label class="control-label" for="<%= slug %>_page_uri">Select a page</label>
 
-	<select data-item-form="[[ slug ]]" name="children[[[ slug ]]][page_uri]" id="[[ slug ]]_page_uri" class="form-control">
+	<select data-item-form="<%= slug %>" name="children[<%= slug %>][page_uri]" id="<%= slug %>_page_uri" class="form-control">
 		@foreach ($pages as $page)
-		<option value="{{ $page->id }}"[? if page_uri == '{{ $page->id }}' ?] selected="selected"[? endif ?]>/{{ $page->uri }}</option>
+		[? if page_uri == '{{ $page->id }}' ?]
+		<option value="{{ $page->id }}" selected="selected">/{{ $page->uri }}</option>
+		[? else ?]
+		<option value="{{ $page->id }}">/{{ $page->uri }}</option>
+		[? endif ?]
 		@endforeach
 	</select>
 </div>
