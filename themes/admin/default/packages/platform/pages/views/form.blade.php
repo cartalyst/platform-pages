bbbe0b8a7d49aaa8038a383c4cfef0b4b5275592
Bruno Gaspar <brunofgaspar1@gmail.com>
2013-12-30 20:13:12 +0000

    push the delete button to the right on the form view
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- themes/admin/default/packages/platform/pages/views/form.blade.php
+++ themes/admin/default/packages/platform/pages/views/form.blade.php
@@ -304,7 +304,9 @@
 					@if ($page->exists and $mode != 'copy')
 					<a class="btn btn-info" href="{{ URL::toAdmin("pages/{$page->slug}/copy") }}">{{{ trans('button.copy') }}}</a>
 
-					<a class="btn btn-danger" data-toggle="modal" data-target="modal-confirm" href="{{ URL::toAdmin("pages/{$page->slug}/delete") }}">{{{ trans('button.delete') }}}</a>
+					<div class="pull-right">
+						<a class="btn btn-danger" data-toggle="modal" data-target="modal-confirm" href="{{ URL::toAdmin("pages/{$page->slug}/delete") }}">{{{ trans('button.delete') }}}</a>
+					</div>
 					@endif
 
 				</div>
