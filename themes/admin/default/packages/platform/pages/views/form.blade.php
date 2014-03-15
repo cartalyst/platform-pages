2eeb5ff93022a9b32b715f309389a9a3a410cd12
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-03-15 03:14:35 +0000

    fix menu children dropdowns not showing up when creating a page
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- themes/admin/default/packages/platform/pages/views/form.blade.php
+++ themes/admin/default/packages/platform/pages/views/form.blade.php
@@ -272,7 +272,7 @@
 
 							@foreach ($menus as $item)
 							<div{{ ( ! empty($menu) and $menu->menu == $item->menu) ? null : ' class="hide"' }} data-menu-parent="{{{ $item->menu }}}">
-								@widget('platform/menus::dropdown.show', [$item->slug, 0, ! empty($menu) ? $menu->getParent()->id : null, ['id' => 'parent_id', 'name' => "parent[{$item->menu}]", 'class' => 'form-control'], ['0' => trans('platform/pages::form.navigation.top_level')]])
+								@widget('platform/menus::dropdown.show', [$item->slug, 0, $menu->exists ? $menu->getParent()->id : null, ['id' => 'parent_id', 'name' => "parent[{$item->menu}]", 'class' => 'form-control'], ['0' => trans('platform/pages::form.navigation.top_level')]])
 							</div>
 							@endforeach
 
