821f9c4385a9ec62a79a854fa4a8accff35cd7f4
drsii <dan.syme@gmail.com>
2015-01-07 13:34:52 -0800

    move langs to app common.

--- themes/admin/default/packages/platform/pages/views/help.blade.php
+++ themes/admin/default/packages/platform/pages/views/help.blade.php
@@ -3,13 +3,13 @@
 	<header class="panel-heading">
 
 		<h4>
-			<i class="fa fa-life-ring" data-toggle="popover" data-content="{{{ trans('platform/content::common.help.setting') }}}"></i> {{{ trans('platform/content::common.help.title') }}}
+			<i class="fa fa-life-ring" data-toggle="popover" data-content="{{{ trans('common.help.setting') }}}"></i> {{{ trans('common.help.title') }}}
 
 			<a class="panel-close small pull-right collapsed tip" data-original-title="{{{ trans('action.collapse') }}}" data-toggle="collapse" href="#help-body" aria-expanded="false" aria-controls="help-body"></a>
 
 			<a class="manual small pull-right" href="{{URL::to('https://cartalyst.com/manual/platform-pages')}}" target="_blank">
 				<i class="fa fa-file-text-o fa-sm"></i>
-				<span>{{{ trans('platform/content::common.help.documentation') }}}</span>
+				<span>{{{ trans('common.help.documentation') }}}</span>
 			</a>
 
 		</h4>
@@ -26,9 +26,11 @@
 
 			</div>
 
-
 		</div>
 
 	</div>
 
 </section>
+
+
+
