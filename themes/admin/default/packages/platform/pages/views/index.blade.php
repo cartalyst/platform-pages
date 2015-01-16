c819a59a6f41457107d5408778cd912c8ecd0b21
Bruno Gaspar <brunofgaspar1@gmail.com>
2015-01-16 02:53:59 +0000

    Update index view.
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- themes/admin/default/packages/platform/pages/views/index.blade.php
+++ themes/admin/default/packages/platform/pages/views/index.blade.php
@@ -95,26 +95,17 @@
 					<form class="navbar-form navbar-right" method="post" accept-charset="utf-8" data-search data-grid="main" role="form">
 
 						@if ( ! empty($tags))
-						<div class="input-group">
+						<div class="btn-group">
 
-							<span class="input-group-btn">
+							<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+								<i class="fa fa-tags"></i> <span class="caret"></span>
+							</button>
 
-								<button class="btn btn-default" type="button" disabled>
-									{{{ trans('platform/media::model.tags') }}}
-								</button>
-
-								<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
-									<span class="caret"></span>
-									<span class="sr-only">Toggle Dropdown</span>
-								</button>
-
-								<ul class="dropdown-menu" role="tags">
-									@foreach ($tags as $tag)
-									<li><a href="#" data-grid="main" data-filter="tags..name:{{{ $tag }}}" data-label="tags..name::{{{ $tag }}}">{{{ $tag }}}</a></li>
-									@endforeach
-								</ul>
-
-							</span>
+							<ul class="dropdown-menu" role="tags">
+								@foreach ($tags as $tag)
+								<li><a href="#" data-grid="main" data-filter="tags..name:{{{ $tag }}}" data-label="tags..name::{{{ $tag }}}">{{{ $tag }}}</a></li>
+								@endforeach
+							</ul>
 
 						</div>
 						@endif
