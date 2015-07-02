f6e7d59b04c27d971e90da87e97f5731cdb83e52
Bruno Gaspar <brunofgaspar1@gmail.com>
2015-07-02 19:52:14 +0100

    Temp fix.
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- themes/admin/default/packages/platform/pages/views/form.blade.php
+++ themes/admin/default/packages/platform/pages/views/form.blade.php
@@ -426,9 +426,11 @@
 
 							@foreach ($menus as $item)
 							<div{!! ($menu->menu == $item->menu) ? null : ' class="hide"' !!} data-menu-parent="{!! $item->menu !!}">
-							@dropdown('platform/menus::dropdown.show', [$item->slug, 0, $menu->exists ? $menu->getParent()->id : null, ['id' => 'parent_id', 'name' => "parent[{$item->menu}]", 'class' => 'form-control'], ['0' => trans('platform/pages::model.navigation.top_level')]])
-						</div>
-						@endforeach
+								<!--
+								dropdown('platform/menus::dropdown.show', [$item->slug, 0, $menu->exists ? $menu->getParent()->id : null, ['id' => 'parent_id', 'name' => "parent[{$item->menu}]", 'class' => 'form-control'], ['0' => trans('platform/pages::model.navigation.top_level')]])
+								-->
+							</div>
+							@endforeach
 
 					</fieldset>
 
